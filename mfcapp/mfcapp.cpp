67cc680e6ff159ba26c72873d9240b8e8aefe93c
k j <you@example.com>
2026-07-23 19:44:16 +0900

    Initial commit

---
+++ mfcapp/mfcapp.cpp
@@ -0,0 +1,137 @@
+﻿
+// mfcapp.cpp : アプリケーションのクラス動作を定義します。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "afxwinappex.h"
+#include "afxdialogex.h"
+#include "mfcapp.h"
+#include "MainFrm.h"
+
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CmfcappApp
+
+BEGIN_MESSAGE_MAP(CmfcappApp, CWinApp)
+	ON_COMMAND(ID_APP_ABOUT, &CmfcappApp::OnAppAbout)
+END_MESSAGE_MAP()
+
+
+// CmfcappApp の構築
+
+CmfcappApp::CmfcappApp() noexcept
+{
+
+	// TODO: 下のアプリケーション ID 文字列を一意の ID 文字列で置換します。推奨される
+	// 文字列の形式は CompanyName.ProductName.SubProduct.VersionInformation です
+	SetAppID(_T("mfcapp.AppID.NoVersion"));
+
+	// TODO: この位置に構築用コードを追加してください。
+	// ここに InitInstance 中の重要な初期化処理をすべて記述してください。
+}
+
+// 唯一の CmfcappApp オブジェクト
+
+CmfcappApp theApp;
+
+
+// CmfcappApp の初期化
+
+BOOL CmfcappApp::InitInstance()
+{
+	CWinApp::InitInstance();
+
+
+	EnableTaskbarInteraction(FALSE);
+
+	// RichEdit コントロールを使用するには AfxInitRichEdit2() が必要です
+	// AfxInitRichEdit2();
+
+	// 標準初期化
+	// これらの機能を使わずに最終的な実行可能ファイルの
+	// サイズを縮小したい場合は、以下から不要な初期化
+	// ルーチンを削除してください。
+	// 設定が格納されているレジストリ キーを変更します。
+	// TODO: 会社名または組織名などの適切な文字列に
+	// この文字列を変更してください。
+	SetRegistryKey(_T("アプリケーション ウィザードで生成されたローカル アプリケーション"));
+
+
+	// メイン ウィンドウを作成するとき、このコードは新しいフレーム ウィンドウ オブジェクトを作成し、
+	// それをアプリケーションのメイン ウィンドウにセットします
+	CFrameWnd* pFrame = new CMainFrame;
+	if (!pFrame)
+		return FALSE;
+	m_pMainWnd = pFrame;
+	// フレームをリソースからロードして作成します
+	pFrame->LoadFrame(IDR_MAINFRAME,
+		WS_OVERLAPPEDWINDOW | FWS_ADDTOTITLE, nullptr,
+		nullptr);
+
+
+
+
+
+	// メイン ウィンドウが初期化されたので、表示と更新を行います。
+	pFrame->ShowWindow(SW_SHOW);
+	pFrame->UpdateWindow();
+	return TRUE;
+}
+
+int CmfcappApp::ExitInstance()
+{
+	//TODO: 追加したリソースがある場合にはそれらも処理してください
+	return CWinApp::ExitInstance();
+}
+
+// CmfcappApp メッセージ ハンドラー
+
+
+// アプリケーションのバージョン情報に使われる CAboutDlg ダイアログ
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg() noexcept;
+
+// ダイアログ データ
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV サポート
+
+// 実装
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() noexcept : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+// ダイアログを実行するためのアプリケーション コマンド
+void CmfcappApp::OnAppAbout()
+{
+	CAboutDlg aboutDlg;
+	aboutDlg.DoModal();
+}
+
+// CmfcappApp メッセージ ハンドラー
+
+
+
---
+++ mfcapp/mfcapp.h
@@ -0,0 +1,35 @@
+﻿
+// mfcapp.h : mfcapp アプリケーションのメイン ヘッダー ファイル
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "resource.h"       // メイン シンボル
+
+
+// CmfcappApp:
+// このクラスの実装については、mfcapp.cpp を参照してください
+//
+
+class CmfcappApp : public CWinApp
+{
+public:
+	CmfcappApp() noexcept;
+
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 実装
+
+public:
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CmfcappApp theApp;
