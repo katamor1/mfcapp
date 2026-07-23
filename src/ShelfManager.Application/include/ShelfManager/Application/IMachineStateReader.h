2b2525b95523cbe9b550b88bbe861de1ccf84e51
katamor1 <a_mysterious_knight@yahoo.co.jp>
2026-07-24 05:34:50 +0900

    docs: define machine state read semantics

--- src/ShelfManager.Application/include/ShelfManager/Application/IMachineStateReader.h
+++ src/ShelfManager.Application/include/ShelfManager/Application/IMachineStateReader.h
@@ -9,6 +9,9 @@ class IMachineStateReader {
 public:
     virtual ~IMachineStateReader() = default;
 
+    // requestで指定した監視区分の状態Fragmentを同期取得する。
+    // 成功結果は取得時点の値であり、MachineSnapshotStoreへの公開は行わない。
+    // 実装は失敗を0、空文字、正常値へ置き換えずResultのErrorとして返す。
     [[nodiscard]] virtual ShelfManager::Domain::Result<MachineSnapshotFragment>
     Read(const MonitoringRequest& request) = 0;
 };
