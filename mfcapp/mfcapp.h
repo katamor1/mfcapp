0b753fc1550543d6995073633c310400e23bd98d
katamor1 <a_mysterious_knight@yahoo.co.jp>
2026-07-24 08:44:52 +0900

    docs: clarify MFC application lifecycle boundary

--- mfcapp/mfcapp.h
+++ mfcapp/mfcapp.h
@@ -1,35 +1,29 @@
-﻿
 // mfcapp.h : mfcapp アプリケーションのメイン ヘッダー ファイル
 //
 #pragma once
 
 #ifndef __AFXWIN_H__
-	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
 #endif
 
-#include "resource.h"       // メイン シンボル
+#include "resource.h"
 
-
-// CmfcappApp:
-// このクラスの実装については、mfcapp.cpp を参照してください
-//
-
-class CmfcappApp : public CWinApp
-{
+// MFC Applicationのライフサイクル入口兼Composition Root。
+// Domain／Applicationの判断をMessage Handlerへ直接実装せず、生成したPort、
+// Presenter、Viewの接続と開始・停止順序をこの境界へ集約する。
+class CmfcappApp : public CWinApp {
 public:
-	CmfcappApp() noexcept;
+    CmfcappApp() noexcept;
 
+    // UI thread上でMFC基盤とMain Frameを初期化する。
+    BOOL InitInstance() override;
 
-// オーバーライド
-public:
-	virtual BOOL InitInstance();
-	virtual int ExitInstance();
+    // Application終了時に所有Resourceを停止・解放してからMFC基盤を終了する。
+    int ExitInstance() override;
 
-// 実装
-
-public:
-	afx_msg void OnAppAbout();
-	DECLARE_MESSAGE_MAP()
+    afx_msg void OnAppAbout();
+    DECLARE_MESSAGE_MAP()
 };
 
+// MFC frameworkが管理するProcess単位のApplication instance。
 extern CmfcappApp theApp;
