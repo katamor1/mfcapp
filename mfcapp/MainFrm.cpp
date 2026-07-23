603d51633332defad124fcc5e0cb6ea1ab70206d
katamor1 <a_mysterious_knight@yahoo.co.jp>
2026-07-24 08:45:36 +0900

    docs: remove main frame wizard comment debt

--- mfcapp/MainFrm.cpp
+++ mfcapp/MainFrm.cpp
@@ -1,97 +1,79 @@
-﻿
 // MainFrm.cpp : CMainFrame クラスの実装
 //
 
 #include "pch.h"
 #include "framework.h"
 #include "mfcapp.h"
-
 #include "MainFrm.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
 
-// CMainFrame
-
 IMPLEMENT_DYNAMIC(CMainFrame, CFrameWnd)
 
 BEGIN_MESSAGE_MAP(CMainFrame, CFrameWnd)
-	ON_WM_CREATE()
-	ON_WM_SETFOCUS()
+    ON_WM_CREATE()
+    ON_WM_SETFOCUS()
 END_MESSAGE_MAP()
 
-// CMainFrame コンストラクション/デストラクション
+CMainFrame::CMainFrame() noexcept = default;
+CMainFrame::~CMainFrame() = default;
 
-CMainFrame::CMainFrame() noexcept
-{
-	// TODO: メンバー初期化コードをここに追加してください。
+int CMainFrame::OnCreate(LPCREATESTRUCT lpCreateStruct) {
+    if (CFrameWnd::OnCreate(lpCreateStruct) == -1) {
+        return -1;
+    }
+
+    // Client領域全体を占める唯一のViewをFrameのChild Windowとして作成する。
+    if (!m_wndView.Create(
+            nullptr,
+            nullptr,
+            AFX_WS_DEFAULT_VIEW,
+            CRect(0, 0, 0, 0),
+            this,
+            AFX_IDW_PANE_FIRST,
+            nullptr)) {
+        TRACE0("ビュー ウィンドウを作成できませんでした。\n");
+        return -1;
+    }
+    return 0;
 }
 
-CMainFrame::~CMainFrame()
-{
+BOOL CMainFrame::PreCreateWindow(CREATESTRUCT& cs) {
+    if (!CFrameWnd::PreCreateWindow(cs)) {
+        return FALSE;
+    }
+
+    // MVP ShellではDocument titleやClient edgeを持たない単一Frameとして生成する。
+    cs.style = WS_OVERLAPPED | WS_CAPTION | FWS_ADDTOTITLE;
+    cs.dwExStyle &= ~WS_EX_CLIENTEDGE;
+    cs.lpszClass = AfxRegisterWndClass(0);
+    return TRUE;
 }
-
-int CMainFrame::OnCreate(LPCREATESTRUCT lpCreateStruct)
-{
-	if (CFrameWnd::OnCreate(lpCreateStruct) == -1)
-		return -1;
-
-	// フレームのクライアント領域全体を占めるビューを作成します。
-	if (!m_wndView.Create(nullptr, nullptr, AFX_WS_DEFAULT_VIEW,
-		CRect(0, 0, 0, 0), this, AFX_IDW_PANE_FIRST, nullptr))
-	{
-		TRACE0("ビュー ウィンドウを作成できませんでした。\n");
-		return -1;
-	}
-	return 0;
-}
-
-BOOL CMainFrame::PreCreateWindow(CREATESTRUCT& cs)
-{
-	if( !CFrameWnd::PreCreateWindow(cs) )
-		return FALSE;
-	// TODO: この位置で CREATESTRUCT cs を修正して Window クラスまたはスタイルを
-	//  修正してください。
-
-	cs.style = WS_OVERLAPPED | WS_CAPTION | FWS_ADDTOTITLE
-		;
-
-	cs.dwExStyle &= ~WS_EX_CLIENTEDGE;
-	cs.lpszClass = AfxRegisterWndClass(0);
-	return TRUE;
-}
-
-// CMainFrame の診断
 
 #ifdef _DEBUG
-void CMainFrame::AssertValid() const
-{
-	CFrameWnd::AssertValid();
+void CMainFrame::AssertValid() const {
+    CFrameWnd::AssertValid();
 }
 
-void CMainFrame::Dump(CDumpContext& dc) const
-{
-	CFrameWnd::Dump(dc);
+void CMainFrame::Dump(CDumpContext& dc) const {
+    CFrameWnd::Dump(dc);
 }
-#endif //_DEBUG
+#endif
 
-
-// CMainFrame メッセージ ハンドラー
-
-void CMainFrame::OnSetFocus(CWnd* /*pOldWnd*/)
-{
-	// ビュー ウィンドウにフォーカスを与えます。
-	m_wndView.SetFocus();
+void CMainFrame::OnSetFocus(CWnd* /*pOldWnd*/) {
+    // Keyboard入力とCommand routingの基点をClient Viewへ維持する。
+    m_wndView.SetFocus();
 }
 
-BOOL CMainFrame::OnCmdMsg(UINT nID, int nCode, void* pExtra, AFX_CMDHANDLERINFO* pHandlerInfo)
-{
-	// ビューに最初にコマンドを処理する機会を与えます。
-	if (m_wndView.OnCmdMsg(nID, nCode, pExtra, pHandlerInfo))
-		return TRUE;
-
-	// それ以外の場合は、既定の処理を行います。
-	return CFrameWnd::OnCmdMsg(nID, nCode, pExtra, pHandlerInfo);
+BOOL CMainFrame::OnCmdMsg(
+    UINT nID,
+    int nCode,
+    void* pExtra,
+    AFX_CMDHANDLERINFO* pHandlerInfo) {
+    if (m_wndView.OnCmdMsg(nID, nCode, pExtra, pHandlerInfo)) {
+        return TRUE;
+    }
+    return CFrameWnd::OnCmdMsg(nID, nCode, pExtra, pHandlerInfo);
 }
-
