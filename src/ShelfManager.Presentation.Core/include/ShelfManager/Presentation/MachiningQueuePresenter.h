2a697e05e4575d16f98293fcf7d0d2fffe5ed6ef
katamor1 <a_mysterious_knight@yahoo.co.jp>
2026-07-24 19:51:05 +0900

    feat: 加工順位Presenter契約を追加

---
+++ src/ShelfManager.Presentation.Core/include/ShelfManager/Presentation/MachiningQueuePresenter.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "ShelfManager/Application/IWorkpieceDetailRequestPort.h"
+#include "ShelfManager/Application/MachineSnapshotStore.h"
+#include "ShelfManager/Application/MoveWorkpiecePriorityUseCase.h"
+#include "ShelfManager/Application/OperationExecutor.h"
+#include "ShelfManager/Application/OperationStateStore.h"
+#include "ShelfManager/Presentation/IMachiningQueueView.h"
+#include "ShelfManager/Presentation/UiStateStore.h"
+
+namespace ShelfManager::Presentation {
+
+// QueuePriority順の一覧、選択Workpieceの指示書列、Up／Down可否を生成する。
+class MachiningQueuePresenter final {
+public:
+    MachiningQueuePresenter(
+        IMachiningQueueView& view,
+        ShelfManager::Application::MachineSnapshotStore& snapshotStore,
+        UiStateStore& uiState,
+        ShelfManager::Application::IWorkpieceDetailRequestPort& detailRequests,
+        ShelfManager::Application::OperationStateStore& operationStateStore,
+        ShelfManager::Application::OperationExecutor& executor,
+        ShelfManager::Application::MoveWorkpiecePriorityUseCase& moveUseCase);
+
+    // THREAD: Public APIはUI threadから直列に呼び出す。
+    void Activate();
+    void OnSnapshotChanged();
+    void OnOperationCompleted(ShelfManager::Application::OperationId operationId);
+    void SelectWorkpiece(ShelfManager::Domain::WorkpieceId workpieceId);
+    void MoveUp();
+    void MoveDown();
+
+private:
+    void SubmitMove(ShelfManager::Domain::MoveDirection direction);
+    [[nodiscard]] MachiningQueueViewModel BuildViewModel();
+
+    IMachiningQueueView& view_;
+    ShelfManager::Application::MachineSnapshotStore& snapshotStore_;
+    UiStateStore& uiState_;
+    ShelfManager::Application::IWorkpieceDetailRequestPort& detailRequests_;
+    ShelfManager::Application::OperationStateStore& operationStateStore_;
+    ShelfManager::Application::OperationExecutor& executor_;
+    ShelfManager::Application::MoveWorkpiecePriorityUseCase& moveUseCase_;
+    std::wstring lastMessage_;
+};
+
+}  // namespace ShelfManager::Presentation
