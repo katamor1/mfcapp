d9c11008de5ffe5364375e45def505c7b4426f6b
katamor1 <a_mysterious_knight@yahoo.co.jp>
2026-07-26 00:42:38 +0900

    feat: inject machine model state into queue presenter

--- src/ShelfManager.Presentation.Core/include/ShelfManager/Presentation/MachiningQueuePresenter.h
+++ src/ShelfManager.Presentation.Core/include/ShelfManager/Presentation/MachiningQueuePresenter.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "ShelfManager/Application/IMachineModelProfileSource.h"
 #include "ShelfManager/Application/IWorkpieceDetailRequestPort.h"
 #include "ShelfManager/Application/MachineSnapshotStore.h"
 #include "ShelfManager/Application/MoveWorkpiecePriorityUseCase.h"
@@ -11,6 +12,7 @@
 namespace ShelfManager::Presentation {
 
 // QueuePriority順の一覧、選択Workpieceの指示書列、Up／Down可否を生成する。
+// 機種未確定・不一致時も閲覧と選択を継続し、変更操作だけを無効化する。
 class MachiningQueuePresenter final {
 public:
     MachiningQueuePresenter(
@@ -20,7 +22,9 @@ public:
         ShelfManager::Application::IWorkpieceDetailRequestPort& detailRequests,
         ShelfManager::Application::OperationStateStore& operationStateStore,
         ShelfManager::Application::OperationExecutor& executor,
-        ShelfManager::Application::MoveWorkpiecePriorityUseCase& moveUseCase);
+        ShelfManager::Application::MoveWorkpiecePriorityUseCase& moveUseCase,
+        const ShelfManager::Application::IMachineModelProfileSource&
+            profileSource);
 
     // THREAD: Public APIはUI threadから直列に呼び出す。
     void Activate();
@@ -41,6 +45,7 @@ private:
     ShelfManager::Application::OperationStateStore& operationStateStore_;
     ShelfManager::Application::OperationExecutor& executor_;
     ShelfManager::Application::MoveWorkpiecePriorityUseCase& moveUseCase_;
+    const ShelfManager::Application::IMachineModelProfileSource& profileSource_;
     std::wstring lastMessage_;
 };
 
